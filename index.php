ec51d79f9ad01e40fb1b5d698635c22a37555df2
Timothe TIZIO <t.tizio@mailbox.org>
2024-04-23 20:03:43 +0200

    Remove useless code

--- index.php
+++ index.php
@@ -8,17 +8,6 @@ use GlimpsGoneV2\core\TemplateEngine;
 // Initialisation du moteur de template
 $templateEngine = new TemplateEngine();
 
-// Chargement du contenu SQL (assurez-vous que le chemin est correct et que le fichier existe)
-$sqlContent = file_get_contents(__DIR__ . '/database/sql/000-install_db.sql');
-if ($sqlContent === false) {
-    die("Erreur lors du chargement du fichier SQL.");
-}
-
-// Configuration initiale du titre de la page (si nécessaire pour le template)
-$title = 'Accueil';
-
-// Rendu du template d'accueil
-$templateEngine->render('accueil.pug', ['title' => $title]);
 
 // Initialisation et démarrage de l'application
 $app = App::getAppInstance();
